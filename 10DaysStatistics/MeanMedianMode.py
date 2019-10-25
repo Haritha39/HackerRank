962436b8c4f16e1f9db5a570abc3686fd043d7ae
Haritha Kommireddy <haritha.kommireddy@xcubelabs.com>
2019-10-25 17:14:37 +0530

    mean,median and mode

--- 10DaysStatistics/MeanMedianMode.py
+++ 10DaysStatistics/MeanMedianMode.py
@@ -1,9 +1,44 @@
+def calculateMean( array ):
+
+    mean = 0
+    # mean = (mean + val for val in array)/len(array)
+    for val in array:
+        mean = mean + val
+    mean = mean/len(array)
+    return mean
+
+def calculateMedian( array ):
+    length = len(array)
+    if(length%2 == 0):
+        array.sort()
+        median = (array[length//2] + array[length//2-1])/2
+    else:
+        median = array[length/2]
+    return median
+
+def calculateMode( array ):
+
+    mode = -1
+    for val in array:
+        mode = val if array.count(val) > array.count(mode) else mode
+    return mode
+
+
 
 N = int(input())
 
 if( N >= 10 and N <= 2500 ):
 
-    X = [ int(input())  for _ in range(N) ]
+    X = input().split(" ")
+    X = [int(n) for n in X]
+
+    mean = calculateMean(X)
+    median = calculateMedian(X)
+    mode = calculateMode(X)
+
+    print( mean )
+    print( median )
+    print( mode )
 
 
-print( X )
+# print( X if X else False)
