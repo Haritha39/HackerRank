05b8176a5c8a5b1eea5606e678a4ac30441b8de2
Haritha Kommireddy <haritha.kommireddy@xcubelabs.com>
2019-10-28 19:11:36 +0530

    quartiles in progress

---
+++ 10DaysStatistics/quartiles.py
@@ -0,0 +1,55 @@
+def calculateMedian( array ):
+    length = len(array)
+    if(length%2 == 0):
+        array.sort()
+        median = (array[length//2] + array[length//2-1])2
+    else:
+        median = array[length//2]
+    return median
+
+def handleEvenLengthArray(array):
+
+    temp = len(array)//2
+    # print(temp)
+    left = array[0:temp]
+    right = array[temp:]
+
+    q1 = calculateMedian(left)
+    q3 = calculateMedian(right)
+
+    # print(q1,q3)
+
+    return [q1,q3]
+
+def handleOddLengthArray( array ):
+
+    temp = len(array)//2
+    # print(temp)
+    left = array[0:temp]
+    right = array[temp+1:]
+
+    q1 = calculateMedian(left)
+    q2 = array[temp]
+    q3 = calculateMedian(right)
+
+    # print(q1,q2,q3)
+
+    return [q1,q2,q3]
+
+
+N = int(input()) 
+if( N >=5 and N <= 50 ):
+    X = input().split(" ")
+    X = [int(val) for val in X]
+    X.sort()
+    # print( X )
+
+    if(N%2 == 0):
+        quaritles = handleEvenLengthArray( X )
+    else:
+        quartiles = handleOddLengthArray( X )
+
+    for q in quartiles:
+        print(q)
+    
+
