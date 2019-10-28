7aafbeaa0bd7902f474095f4773e9a0bea2fc644
Haritha Kommireddy <haritha.kommireddy@xcubelabs.com>
2019-10-28 15:01:26 +0530

    Wrong weighted mean

---
+++ 10DaysStatistics/weightedMean.py
@@ -0,0 +1,38 @@
+def calculateWeightedMean( values , weights ):
+
+    """ 
+    Formula :
+
+        sigma i(values(i) * weights(i) )
+        ________________________________
+                sigma(weights)
+    
+    """
+    numerator = 0
+    for i in range(len(values)):
+
+        numerator = numerator + (values[i]*weights[i])
+
+    denominator = sum(weights)
+
+    weightedMean = numerator / denominator
+
+    return weightedMean
+
+    
+
+
+
+N = int(input())
+
+if( N >=5 and N <=50 ):
+    
+    X = input().split(" ")
+    X = [int(val) for val in X]
+
+    W = input().split(" ")
+    W = [int(val) for val in W]
+
+    weightedMean = calculateWeightedMean(X ,W)
+
+    print(weightedMean)
